28d924757c1ad740bf6f3c03d8652fab7658a6e7
beckhsu <justin7610@gmail.com>
2019-12-25 17:50:20 +0800

    seccommit

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,6 +7,11 @@ require("@rails/ujs").start()
 require("@rails/activestorage").start()
 require("channels")
 
+import 'popper.js'
+import 'jquery'
+import 'bootstrap'
+import '../stylesheet/index'
+
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
